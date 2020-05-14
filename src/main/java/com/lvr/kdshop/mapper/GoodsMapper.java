dba21b34b5cd5fcb3576a8be025d572fcc32e9f2
lvr1997 <lr1997rrrrr@163.com>
2020-05-14 17:53:40 +0800

    去掉无用代码

--- src/main/java/com/lvr/kdshop/mapper/GoodsMapper.java
+++ src/main/java/com/lvr/kdshop/mapper/GoodsMapper.java
@@ -1,9 +1,7 @@
 package com.lvr.kdshop.mapper;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lvr.kdshop.entity.Goods;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lvr.kdshop.util.MyPage;
 
 /**
  * <p>
@@ -15,11 +13,4 @@ import com.lvr.kdshop.util.MyPage;
  */
 public interface GoodsMapper extends BaseMapper<Goods> {
 
-    /**
-     * 查询： 根据state状态查询商品列表，分页显示
-     * @param page 分页对象,xml中可以从里面进行取值,传递参数 Page 即自动分页,必须放在第一位(你可以继承Page实现自己的分页对象)
-     * @param state 状态
-     * @return分页对象
-     */
-    MyPage<Goods> selectPageVo(Page<?> page, Integer state);
 }
