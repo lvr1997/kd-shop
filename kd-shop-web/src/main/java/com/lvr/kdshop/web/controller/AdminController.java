6ffb7a6ce1a05c4ee709456906520222ad40714c
梨花初现 <lr1997rrrrr@163.com>
2024-09-19 17:32:13 +0800

    feat: 前端管理员静态页面配置

---
+++ kd-shop-web/src/main/java/com/lvr/kdshop/web/controller/AdminController.java
@@ -0,0 +1,17 @@
+package com.lvr.kdshop.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("msg", "Hello 欢迎回来");
+        return "/index";
+    }
+}
